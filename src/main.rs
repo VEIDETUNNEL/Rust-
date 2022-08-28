c72c67f72a4438bea64f05a80271e32b5b44eb0f
VEIDETUNNEL <84806472+VEIDETUNNEL@users.noreply.github.com>
2022-08-28 12:07:24 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,35 @@
+// #[allow(dead_code)]
 #[allow(dead_code)]
-fn main() {
-    let _age = "10";
-    let _name = "veide";
+#[allow(unused_mut)]
 
-    println! { "{}{}", _name , _age }
+pub fn main() {
+    /*
+    let mut string = String::from("gunnal was here");
+
+    string.push_str("ehllo");
+
+    let mut x: bool = true;
+    let c: char = '\u{1F3B8}';
+
+    let max = std::i128::MAX;
+
+    println!("{:?}", (max, x, c, string));
+    {
+        let num: i32 = 12;
+
+        println!("{}", num);
+    }
+    let spaces: &str = "    ";
+    println!("{}", spaces.len())
+        */
+
+    let mut arr: [i32; 6] = [1, 65, -12, 56, 234, 1];
+    let mut i = 0;
+    while i < arr.len() {
+        println!("{}", arr[i]);
+        if arr[i] < 0 {
+            continue;
+        }
+        i = i + 1;
+    }
 }
